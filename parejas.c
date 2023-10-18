e27ac99fc76a5dfe30dfcf4a601dd5354ce2ed57
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-18 15:33:27 -0500

    Proceso terminado, listo para imprimir el resultado

--- parejas.c
+++ parejas.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-    int num1, pareja;
+    int num1, pareja=1;
     for (num1= 0; num1 < 10; num1++){
-        
+        printf("%d %d\n", num1, pareja);
         if (num1 % 2 !=0){
             pareja++;
         }
