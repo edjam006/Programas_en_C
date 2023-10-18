53ac82909768798b4bc7fa45de7d223cad5e178b
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-18 15:56:21 -0500

    Programa terminado, junto a comentarios asignados

--- fibonacci.c
+++ fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main (void){
-    int num= 0, nuevo, sum;
-
-     for (nuevo= 1; nuevo <= 11000;){ 
-            
-            sum= nuevo + num;
-            num= nuevo;
-            nuevo= sum;
+    int num= 0, nuevo, sum; //Declaracion de Variables
+     printf("%d\n", num); //Impresion del numero 0 para que inicie la serie de Fibonacci
+     for (nuevo= 1; nuevo <= 11000;){ //Se otorga el valor de 1 a la variable nuevo, y se plantea la condicion de que siga operando mientras sea <= 11000
+            printf("%d\n", nuevo); //Esta impresion muestra cada resultado que queremos de la serie 
+            sum= nuevo + num; //Esta operacion realiza la suma de los dos ultimos numeros
+            num= nuevo; //Al numero se le otorga el valor de la variable nuevo
+            nuevo= sum; //Aqui la variable nuevo adquiere el nuevo valor de la suma, para que se vuelva a realizar el for y obtener los nuevos resultados 
         }
 }
