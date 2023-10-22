181f7b252b998e83cb5e61e5a523600df32a438e
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-22 08:15:36 -0500

    Declaración de variables y primera operacion para sumar los digitos dentro de un do while

--- sumaD.c
+++ sumaD.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main (void){
-    
+    int num, sum;
+
+    printf("Ingrese un numero entero\n");
+    scanf("%d", &num);
+
+    do {
+        sum= sum + (num % 10);
+    }
 }
