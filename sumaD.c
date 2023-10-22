c837a393528daa883ded61c82d3fb90c504e64e0
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-22 08:18:54 -0500

    Prueba de resultados e impresiones

--- sumaD.c
+++ sumaD.c
@@ -5,8 +5,10 @@ int main (void){
 
     printf("Ingrese un numero entero\n");
     scanf("%d", &num);
-
+    sum= 0;
     do {
         sum= sum + (num % 10);
-    }
+        num= num/10;
+    } while (num != 0);
+    printf("La suma de los digitos del numero %d es: %d", num, sum);
 }
