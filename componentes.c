dfe6f333b23d960cdc40bd1225ffbecc83d26fdf
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-22 07:32:56 -0500

    Primera version con ciclo For

--- componentes.c
+++ componentes.c
@@ -4,4 +4,10 @@ int main (void){
     int i, num;
     printf("Ingrese un numero entero\n");
     scanf("%d", num);
+
+    for (i=0; i<= num; i++){
+        if(num % i == 0){
+            printf("%d", i);
+        }
+    }
 }
