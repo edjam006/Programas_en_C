613512f361757b0d043b65c86a54d9a794ecbd8e
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-22 16:17:14 -0500

    Impresion y funciones de mostrar el resultado completadas

--- buscarD.c
+++ buscarD.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main (void){
-    int num, digito;
+    int num, digito, num1= 0;
 
     printf("Ingrese un numero entero de 3 cifras\n");
     scanf("%d", &num);
@@ -9,10 +9,19 @@ int main (void){
     if (num > 99 && num < 1000){
         do{
             digito= num % 10;
+            if (digito == 1){
+                num1= 1;
+            } else {
+                num1= 0;
+            }
         } while (num > 0);
        
     } else {
         printf("El numero ingresado no cumple la condicion de 3 digitos\n");
     }
-   
+    if (num1 == 1){
+         printf("El numero si tiene el digito 1.\n");
+    } else {
+            printf("El numero no tiene digito 1.\n");
+    }
 }
