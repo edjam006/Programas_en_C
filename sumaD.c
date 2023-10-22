9de8239c31086a4ed7abd9e02fd0e3713842a136
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-10-22 08:22:36 -0500

    Se añadio una nueva variable para almacenar num y que este no cambie su valor al final, programa terminado

--- sumaD.c
+++ sumaD.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main (void){
-    int num, sum;
+    int num, sum, numS;
 
     printf("Ingrese un numero entero\n");
     scanf("%d", &num);
+    numS= num;
     sum= 0;
     do {
-        sum= sum + (num % 10);
-        num= num/10;
-    } while (num != 0);
+        sum= sum + (numS % 10);
+        numS= numS/10;
+    } while (numS != 0);
     printf("La suma de los digitos del numero %d es: %d", num, sum);
 }
